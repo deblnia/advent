c5ee6d9192be5f992fddcc55016d16e6112f4a2a
Deblina Mukherjee <deblina@uchicago.edu>
2020-12-01 13:33:51 -0500

    fixed repeating

--- 2020/day-one/dayone.py
+++ 2020/day-one/dayone.py
@@ -1,16 +1,21 @@
 import numpy as np
 l = list(np.recfromtxt('input.txt'))
 
-# question 1
-for i in l:
-    for j in l:
-        if i + j == 2020:
-            print(i*j)
+# question 1 
+def q1(l): 
+    for i in l:
+        for j in l:
+            if i + j == 2020:
+                print(i*j)
+                return
+q1(l) 
 
-
-# question 2 
-for i in l:
-    for j in l: 
-        for k in l: 
-            if i + j + k == 2020: 
-                print(i*j*k)
+# question 2
+def q2(l):  
+    for i in l:
+        for j in l: 
+            for k in l: 
+                if i + j + k == 2020: 
+                    print(i*j*k)
+                    return
+q2(l)
