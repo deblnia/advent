8c35d629e9f49d956967afb80e499b0c46e5cec9
Deblina Mukherjee <deblina@uchicago.edu>
2020-12-09 14:59:42 -0500

    fix r code

--- 2020/day-two/daytwo.R
+++ 2020/day-two/daytwo.R
@@ -1,18 +1,43 @@
 library(tidyverse)
 
-l <- read.delim("input.txt", header = F, sep = '\n')
-
-nrow(l)    
+l <- readLines("input.txt") #readLines 
+    
 head(l)
 
-# why doesn't this work ? 
-l %>% 
-    separate(V1, into = c("bounds", "letter", "pattern"), sep = " ") %>% 
-    separate(bounds, into = c("lower", "upper"), sep = "-") %>%
-    mutate(letter = gsub(":", "", letter), 
-    count = str_count(pattern, letter), 
-    good = if_else((count >= lower & count <= upper), 1, 0)) %>% 
-    count(good)
+# part 1
+
+
+# l %>% 
+#     separate(V1, into = c("bounds", "letter", "pattern"), sep = " ") %>% 
+#     separate(bounds, into = c("lower", "upper"), sep = "-") %>%
+#     mutate(letter = gsub(":", "", letter), 
+#     count = str_count(pattern, letter), 
+#     good = if_else((count >= lower & count <= upper), 1, 0)) %>% 
+#     count(good)
+
+check <- function(x) {
+  numbers <- as.numeric(str_extract_all(x, "[0-9]+")[[1]])
+  string <- str_extract_all(x, "[a-z]+")[[1]]
+  
+  count <- str_count(string[2], string[1])
+  (numbers[1] <= count) & (numbers[2] >= count) 
+}
+sum(map_lgl(l, check))
+
+
+# part 2 
+
+check_2 <- function(x) {
+  numbers <- as.numeric(str_extract_all(x, "[0-9]+")[[1]])
+  chars <- str_extract_all(x, "[a-z]+")[[1]]
+  
+  loc1 <- str_sub(chars[2], numbers[1], numbers[1])
+  loc2 <- str_sub(chars[2], numbers[2], numbers[2])
+     
+  sum(loc1 == chars[1], loc2 == chars[1]) == 1
+  
+}
+sum(map_lgl(l, check_2))
 
 
 
